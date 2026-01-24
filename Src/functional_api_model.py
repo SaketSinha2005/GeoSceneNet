110a6e60cf212fe6b5b9390f9c847ed00dbfd680
SaketSinha2005 <ss691@snu.edu.in>
2026-01-25 02:43:08 +0530

    update functional api model

--- Src/functional_api_model.py
+++ Src/functional_api_model.py
@@ -2,22 +2,18 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 def build_functional(input_shape = (150, 150, 3), num_classes=6, learning_rate=1e-3):
-    norm_layer = layers.Normalization()
-    hidden_layer1 = layers.Dense(64, activation='relu')
-    hidden_layer2 = layers.Dense(64, activation='relu')
-    concat_layer = layers.Concatenate()
-    hidden_layer3 = layers.Dense(32, activation='relu')
-    output_layer = layers.Dense(num_classes, activation='softmax')
-
     input_ = layers.Input(shape=input_shape)
-    normalized = norm_layer(input_)
-    hidden1 = hidden_layer1(normalized)
-    hidden2 = hidden_layer2(hidden1)
-    concat = concat_layer([hidden2, normalized])
-    hidden3 = hidden_layer3(concat)
-    output = output_layer(hidden3)
+    norm_layer = layers.Normalization()
+    x = norm_layer(input_)
 
-    model = tf.keras.Model(inputs=[input_], outputs=output)
+    flatten = layers.Flatten()(x)
+    hidden_layer1 = layers.Dense(64, activation='relu')(flatten)
+    hidden_layer2 = layers.Dense(64, activation='relu')(hidden_layer1)
+    concat_layer = layers.Concatenate()([hidden_layer2, flatten])
+    hidden_layer3 = layers.Dense(32, activation='relu')(concat_layer)
+    output_layer = layers.Dense(num_classes, activation='softmax')(hidden_layer3)
+
+    model = tf.keras.Model(inputs=[input_], outputs=output_layer)
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
     model.compile(
