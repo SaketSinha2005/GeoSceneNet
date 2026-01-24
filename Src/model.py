ef34fd8a79b942de8b33e47e2f88c1ada54f04c2
SaketSinha2005 <ss691@snu.edu.in>
2026-01-24 21:23:27 +0530

    update files

--- Src/model.py
+++ Src/model.py
@@ -2,21 +2,24 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 def build_mlp(input_shape=(150, 150, 3), num_classes=6, learning_rate=1e-3):
-    norm_layer = layers.Normalization(input_shape=input_shape)    
+    norm_layer = layers.Normalization(input_shape=input_shape)
     model = tf.keras.Sequential([
         norm_layer,
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
         layers.Dense(64, activation='relu'),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(16, activation='relu'),
         layers.Dense(num_classes, activation='softmax')
     ])
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     model.compile(
-        optimizer=optimizer, 
-        loss='SparseCategoricalCrossentropy', 
+        optimizer=optimizer,
+        loss='SparseCategoricalCrossentropy',
         metrics=['accuracy']
     )
 
     return model, norm_layer
-    
